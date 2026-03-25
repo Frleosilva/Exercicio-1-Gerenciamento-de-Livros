9b1753030773984327bcb06a8424bb8107d3814b
Frleosilva <leonardofratonibs@gmail.com>
2026-03-25 20:02:43 -0300

    controller criado

--- src/main/java/com/ex01/atividade01/Repositories/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ex01.atividade01.Repositories;
-
-import com.ex01.atividade01.Models.LivroModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<LivroModel, Long> {
-}
